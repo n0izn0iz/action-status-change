29cbe5a009e4d8c68e331deb7786265a550d088e
jembijemb <jembi@protonmail.com>
2019-07-24 23:29:41 +0200

    feat: require the github token to provide better feedback

--- index.js
+++ index.js
@@ -24,21 +24,24 @@ const updateStatus = ({
     })
   });
 
-Toolkit.run(async tools => {
-  tools.log("arguments", tools.arguments);
-  const argsList = tools.arguments._;
-  if (argsList.length < 2) {
-    tools.exit.failure("Invalid usage: requires context and state arguments");
-    return;
-  }
-  const response = await updateStatus({
-    context: argsList[0],
-    state: argsList[1],
-    description: "A wild action appeared",
-    targetURL: "https://bit.ly/4kb77v",
-    repo: tools.context.repo,
-    sha: tools.context.sha,
-    token: tools.token
-  });
-  tools.exit.success("We did it!");
-});
+Toolkit.run(
+  async tools => {
+    tools.log("arguments", tools.arguments);
+    const argsList = tools.arguments._;
+    if (argsList.length < 2) {
+      tools.exit.failure("Invalid usage: requires context and state arguments");
+      return;
+    }
+    const response = await updateStatus({
+      context: argsList[0],
+      state: argsList[1],
+      description: "A wild action appeared",
+      targetURL: "https://bit.ly/4kb77v",
+      repo: tools.context.repo,
+      sha: tools.context.sha,
+      token: tools.token
+    });
+    tools.exit.success("We did it!");
+  },
+  {secrets: ["GITHUB_TOKEN"]}
+);
