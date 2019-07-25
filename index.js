5aac71193225bbb421c251cb351260b82a11f7ce
jembijemb <jembi@protonmail.com>
2019-07-25 10:06:17 +0200

    refacto: validate state key and log more

--- index.js
+++ index.js
@@ -24,24 +24,37 @@ const updateStatus = ({
     })
   });
 
+const VALID_STATES = ["pending", "success", "error", "failure"];
+
 Toolkit.run(
   async tools => {
-    tools.log("arguments", tools.arguments);
-    const argsList = tools.arguments._;
-    if (argsList.length < 2) {
-      tools.exit.failure("Invalid usage: requires context and state arguments");
-      return;
+    try {
+      const argsList = tools.arguments._;
+      if (argsList.length < 2)
+        throw new Error("Invalid usage: requires context and state arguments");
+
+      const state = argsList[1];
+      if (!VALID_STATES.includes(state))
+        throw new Error(
+          `Invalid state argument: must be one of ${VALID_STATES}`
+        );
+
+      const args = {
+        context: argsList[0],
+        state,
+        description: "A wild action appeared",
+        targetURL: "https://bit.ly/4kb77v",
+        repo: tools.context.repo,
+        sha: tools.context.sha
+      };
+      tools.log("Payload:", args);
+
+      const response = await updateStatus({...args, token: tools.token});
+
+      tools.exit.success("Status updated:", response);
+    } catch (error) {
+      tools.exit.failure(error);
     }
-    const response = await updateStatus({
-      context: argsList[0],
-      state: argsList[1],
-      description: "A wild action appeared",
-      targetURL: "https://bit.ly/4kb77v",
-      repo: tools.context.repo,
-      sha: tools.context.sha,
-      token: tools.token
-    });
-    tools.exit.success("We did it!");
   },
   {secrets: ["GITHUB_TOKEN"]}
 );
